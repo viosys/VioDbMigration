09d2a7e563600956ccdac616d30220599abc2c36
Sebastian König <skoenig@viosys.com>
2017-11-28 08:28:29 +0100

    Versionnummer wird nun in DB-Tabelle gespeichert und nicht mehr in version.txt

--- Components/Migrations/VioManager.php
+++ Components/Migrations/VioManager.php
@@ -17,6 +17,8 @@ class VioManager extends Manager {
     /** @var  Container $container */
     protected $container;
 
+    protected static $tableName =  'vio_db_migration_schema_version';
+
     public function __construct(\PDO $connection, $migrationPath, Container $container)
     {
         parent::__construct($connection, $migrationPath);
@@ -25,10 +27,28 @@ class VioManager extends Manager {
 
     public function createSchemaTable()
     {
-
+        $sql = '
+            CREATE TABLE IF NOT EXISTS `' . static::$tableName . '` (
+            `version` int(11) NOT NULL,
+            `start_date` datetime NOT NULL,
+            `complete_date` datetime DEFAULT NULL,
+            `name` VARCHAR( 255 ) NOT NULL,
+            `error_msg` varchar(255) DEFAULT NULL,
+            PRIMARY KEY (`version`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+        ';
+        $this->connection->exec($sql);
+        // migrate from version.txt
+        $txtVersion = intval($this->getCurrentVersionFromVersionTxt());
+        if($txtVersion > 0) {
+            $sql = "INSERT INTO " . static::$tableName . " (version, start_date, complete_date, name)
+                      VALUES ({$txtVersion}, NOW(), NOW(), 'Version from old version.txt') ";
+            $this->connection->exec($sql);
+            $this->deleteVersionTxt();
+        }
     }
 
-    public function getCurrentVersion()
+    private function getCurrentVersionFromVersionTxt()
     {
         $path = $this->getMigrationPath();
         $path .= DIRECTORY_SEPARATOR;
@@ -42,13 +62,26 @@ class VioManager extends Manager {
         return $version;
     }
 
-    public function setCurrentVersion($version)
+    private function deleteVersionTxt()
     {
         $path = $this->getMigrationPath();
         $path .= DIRECTORY_SEPARATOR;
         $path .= 'version.txt';
+        unlink($path);
+    }
 
-        file_put_contents($path, $version);
+
+    /**
+     * Returns current schma version found in database
+     *
+     * @return int
+     */
+    public function getCurrentVersion()
+    {
+        $sql = 'SELECT version FROM ' . static::$tableName . ' WHERE complete_date IS NOT NULL ORDER BY version DESC';
+        $currentVersion = (int) $this->connection->query($sql)->fetchColumn();
+
+        return $currentVersion;
     }
 
     /**
@@ -60,8 +93,13 @@ class VioManager extends Manager {
      */
     public function apply(AbstractMigration $migration, $modus = AbstractMigration::MODUS_INSTALL)
     {
-        $oldVersion = $this->getCurrentVersion();
-        $this->setCurrentVersion($migration->getVersion());
+        $sql = 'REPLACE ' . static::$tableName . ' (version, start_date, name) VALUES (:version, :date, :name)';
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([
+            ':version' => $migration->getVersion(),
+            ':date' => date('Y-m-d H:i:s'),
+            ':name' => $migration->getLabel(),
+        ]);
 
         try {
             if($migration instanceof VioAbstractMigration){
@@ -75,13 +113,22 @@ class VioManager extends Manager {
                 $this->connection->exec($sql);
             }
         } catch (\Exception $e) {
-
-            $this->setCurrentVersion($oldVersion);
+            $updateVersionSql = 'UPDATE ' . static::$tableName . ' SET error_msg = :msg WHERE version = :version';
+            $stmt = $this->connection->prepare($updateVersionSql);
+            $stmt->execute([
+                ':version' => $migration->getVersion(),
+                ':msg' => $e->getMessage(),
+            ]);
             throw new \Exception(sprintf(
                 'Could not apply migration (%s). Error: %s ', get_class($migration), $e->getMessage()
             ));
         }
-
+        $sql = 'UPDATE ' . static::$tableName . ' SET complete_date = :date WHERE version = :version';
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([
+            ':version' => $migration->getVersion(),
+            ':date' => date('Y-m-d H:i:s'),
+        ]);
     }
 
     /**
